f218c5f5bbf838f02dc679634598f1a8030f2af0
Florent <66668699+florentLutz@users.noreply.github.com>
2026-01-26 15:15:32 +0100

    Finalized

---
+++ laboratories/animation_hybrid_kodiak_100_scheme_C.py
@@ -0,0 +1,26 @@
+# This file is part of FAST-OAD_CS23-HE : A framework for rapid Overall Aircraft Design of Hybrid
+# Electric Aircraft.
+# Copyright (C) 2022 ISAE-SUPAERO
+
+import pathlib
+
+from fastga_he.gui.power_train_network_viewer import power_train_network_viewer
+
+DATA_FOLDER_PATH = pathlib.Path(__file__).parent / "data"
+RESULT_FOLDER_PATH = pathlib.Path(__file__).parent / "results"
+
+if __name__ == "__main__":
+    # Define path to the required files
+    path_to_powertrain_description_file = pathlib.Path(DATA_FOLDER_PATH) / "hybrid_kodiak_100_scheme_C_propulsive_architecture.yml"
+    path_to_network_viewer_file = pathlib.Path(RESULT_FOLDER_PATH) / "hybrid_kodiak_100_scheme_C_network_viewer.html"
+    path_to_powertrain_data_file = pathlib.Path(RESULT_FOLDER_PATH) / "hybrid_kodiak_100_scheme_C_propulsion_data.csv"
+
+    # Generate the network viewer file
+    power_train_network_viewer(
+        path_to_powertrain_description_file,
+        path_to_network_viewer_file,
+        animated_plot=True,
+        orientation="LR",
+        legend_position="TL",
+        pt_watcher_path=path_to_powertrain_data_file
+    )
